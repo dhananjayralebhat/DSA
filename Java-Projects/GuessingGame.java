c120da76270fbe24e29905e3b537c126e88f7c91
Dhananjay Dadasaheb Ralebhat <167185340+dhananjayralebhat@users.noreply.github.com>
2026-01-06 11:03:23 +0530

    Add files via upload

---
+++ Java-Projects/GuessingGame.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class GuessingGame {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int myNumber = (int)(Math.random()*100);
+        int userNumber = 0;
+
+        do{
+            System.out.println("Guess the Number");
+            userNumber = sc.nextInt();
+
+            if(userNumber == myNumber){
+                System.out.println("Correct Number");
+                break;
+            }else if(userNumber > myNumber){
+                System.out.println("your number is too large");
+            }else{
+                System.out.println("your number is too small");
+            }
+        }while(userNumber >= 0);
+        System.out.println("my number was:");
+        System.out.println(myNumber);
+    }
+}
