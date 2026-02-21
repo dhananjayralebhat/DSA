38802a67fabbf1e0387df98b1304fa0dfeaabd93
Dhananjay Dadasaheb Ralebhat <167185340+dhananjayralebhat@users.noreply.github.com>
2026-02-21 23:16:28 +0530

    Add files via upload

---
+++ Advance/TappingRainWater.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class TappingRainWater{
+
+    public static int trappedWater(int height[]){
+        int leftMax[] = new int[height.length];
+        leftMax[0] = height[0];
+        for(int i=1; i<height.length; i++){
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+
+        int rightMax[] = new int[height.length];
+        rightMax[height.length-1] = height[height.length-1];
+        for(int i=height.length-2; i>=0; i--){
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        }
+
+        int trappedWater = 0;
+
+        for(int i=0; i<height.length; i++){
+            int maxWater = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += maxWater - height[i];
+        }
+        return trappedWater;
+        
+    }
+    public static void main(String[] args) {
+        int height[] = {2, 1, 0, 5, 3, 1, 4};
+        System.out.println(trappedWater(height));
+    }
+}
